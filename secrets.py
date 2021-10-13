b944f73a36b9ce46a99e4e4f7a029ba22246f564
Travis Howse <tjhowse@gmail.com>
2021-10-13 10:51:47 +1000

    more

--- secrets.py
+++ secrets.py
@@ -1,2 +1,8 @@
 grocy_api_key = "woo"
 grocy_domain = "https://my.grocy.url.com"
+
+mqtt_host = "192.168.1.1"
+mqtt_user = "user"
+mqtt_pw = "password"
+mqtt_topic_button = "recept_printer/button"
+mqtt_topic_print = "recept_printer/print_text"
